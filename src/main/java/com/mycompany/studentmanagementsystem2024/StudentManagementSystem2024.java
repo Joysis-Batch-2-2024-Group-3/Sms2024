4c636a96ab1b592b02712d53bfa2b03feb17feca
mark <markbobis173@gmail.com>
2024-10-27 08:57:15 +0800

    Project Structure Created

---
+++ src/main/java/com/mycompany/studentmanagementsystem2024/StudentManagementSystem2024.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.studentmanagementsystem2024;
+import View.Index;
+
+/**
+ *
+ * @author mark
+ */
+public class StudentManagementSystem2024 {
+
+    public static void main(String[] args) {
+        Index.SayHello();
+        
+    }
+}
